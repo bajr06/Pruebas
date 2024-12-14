ed7ad615297365b1204d75cf4bcf35214504fee8
bajr06 <181849388+bajr06@users.noreply.github.com.>
2024-12-14 20:30:54 +0100

    Recuperación

---
+++ Python/Variables.py
@@ -0,0 +1,91 @@
+# Como se crea, da valor, e imprime por pantalla una variables.
+var = 10
+print(var)
+
+# Ejemplo
+identificador = 1
+balance_cuenta = 1000.0
+nombre_cliente = 'John Doe'
+print(identificador, balance_cuenta, nombre_cliente)
+print(var)
+
+# Error
+# print(Var)
+
+# Mostrar cadenas con variables
+version = "3.8.5"
+print("Version de Python: " + version)
+
+# Asiganr valor a variable ya creada.
+sus = 1
+print(sus)
+sus = sus + 1
+print(sus)
+
+# Reasignación de valor a una variable
+var1 = 100
+var1 = 200 + 300
+print(var1)
+
+# Resolución de problemas matemáticos simples
+a = 3.0
+b = 4.0
+c = (a ** 2 + b ** 2) ** 0.5
+print("c =", c)
+
+# LABORATORIO
+john = 3
+mary = 5
+adam = 6
+print("Juan:", john, "\nMaría:", mary, "\nAdán:", adam)
+
+total_apples = john + mary + adam
+print("Total de manzanas:", total_apples)
+
+# Prueba
+z = 1
+z = z * 2
+z *= 2 # Abreviación de la línea anterior
+
+sheep = 2
+sheep = sheep + 1
+sheep += 1 # Abreviación de la línea anterior
+
+print(z, sheep)
+
+# De kilometrso a millas y viceversa
+kilometers = 12.25
+miles = 7.38
+
+miles_to_kilometers = miles * 1.61
+kilometers_to_miles = kilometers / 1.61
+
+print(miles, "millas son", miles_to_kilometers, "kilómetros")
+print(kilometers, "kilómetros son", round(kilometers_to_miles, 2), "millas")
+
+# Operaciones algebraicas
+x = 1
+x = float(x)
+y = 3 * x ** 2 - 2 * x ** 2 + 3 * x -1
+print("y =", y)
+
+# Penúltimo ejemplo de asignaciones de variables
+var3 = 2
+print(var3)
+
+var3 = 3
+print(var3)
+
+var3 += 1
+print(var3)
+
+# Combinación de cadena de carácteres con otras cadenas
+var4 = "007"
+print("Agent " + var4)
+
+# Prueba 2
+e = '1'
+f = "1"
+print(e + f)
+
+
