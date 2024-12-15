1fc7546a5cf8a4c26f60069b612492051c304c1b
bajr06 <181849388+bajr06@users.noreply.github.com.>
2024-12-15 11:53:07 +0100

    Comentarios

---
+++ Python/Comentarios.py
@@ -0,0 +1,19 @@
+# Este programa evalúa la hipotenusa c.
+# a y b son las longitudes de los cátetos.
+a = 3.0
+b = 4.0
+c = (a ** 2 + b ** 2) ** 0.5  # Se emplea ** en lugar de una raíz cuadrada.
+print("c =", c)
+
+# Este es un programa de prueba.
+x = 1
+y = 2
+# y = y + x
+print(x + y)
+
+#este programa calcula los segundos en cierto número de horas determinadas
+horas = 2 # número de horas
+segundos = 3600 # número de segundos en una hora
+
+print("Horas: ", horas) #imprime el numero de horas
+# print("Segundos en Horas: ", horas * segundos) # se imprime el numero de segundos en determinado numero de horas
