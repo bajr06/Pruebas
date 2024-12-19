5bcda187f71c165940079f2d5c90502a19172490
bajr06 <bryanandreujimenezrojas@gmail.com>
2024-12-19 13:50:08 +0100

    Módulo 3.7

--- Python/Listas/Operaciones/LAB.py
+++ Python/Listas/Operaciones/LAB.py
@@ -1,17 +1,17 @@
 my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
-print("Esta es la lista actual:", my_list)
-question = input("¿Deseas que no se repitan los números? ")
+print("Lista actual:", my_list)
 
-if question:
-    elem = my_list[0]
+question = input("¿Desear borrar las repeticiones de la lista? ")
 
-    for i in range(len(my_list)):
-        if i == elem:
-            elem += 1
+if question == "Si":
+    new_list = []
 
+    for i in my_list:
+        if i not in new_list:
+            new_list.append(i)
+            my_list = new_list
 else:
-        print("Entendido.")
-
+    print("No se ha realizado la limpia de lista.")
 
 print("La lista con elementos únicos:")
 print(my_list)
