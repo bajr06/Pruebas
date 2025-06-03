e84b50e751b8847142849f85a2ae29c595f7cb84
bajr06 <bryanandreujimenezrojas@gmail.com>
2025-06-03 12:51:18 +0200

    Arreglo C++

--- HolaMundo.cpp
+++ HolaMundo.cpp
@@ -1,5 +1,11 @@
-#include <iostream.h>
+#include <iostream>
+using namespace std;
 
 int main() {
+	cout << "¡Hola, mundo!" << endl;
+
+	cout << "Este mensaje ";
+	cout << "está en la misma línea." << endl;
+
 	return EXIT_SUCCESS;
 }
