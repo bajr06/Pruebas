daf96c91e37c8efe4d2d142ad9de7411941d750d
bajr06 <181849388+bajr06@users.noreply.github.com.>
2025-06-07 02:01:21 +0200

    Cambios

--- Python/Condicionales.py
+++ Python/Condicionales.py
@@ -1,113 +1,17 @@
-#
-#   EJEMPLOS DE CONDICIONALES IF
-#
+print("Hola Mundo")
+print("ESTO ES UNA PRUEBA DEL FUNCIONAMIENTO DE GIT.")
 
-#if sheep_counter >= 120: # Evaluar una expresión condicional
-#   sleep_and_dream() # Ejecutar si la expresión condicional es verdadera
+num_1 = int(input("Escriba un número: "))
+num_2 = int(input("Escriba otro número: "))
 
-# if sheep_counter >= 120:
-#    make_a_bed()
-#    take_a_shower()
-#    sleep_and_dream()
-#feed_the_sheepdogs()
-
-
-#
-#   EJEMPLOS DE CONDICIONALES IF / ELSE
-#
-
-#if true_or_false_condition:
-#    perform_if_condition_true
-#else:
-#    perform_if_condition_false
-
-#if the_weather_is_good:
-#    go_for_a_walk()
-#    have_fun()
-#else:
-#    go_to_a_theater()
-#    enjoy_the_movie()
-#have_lunch()
-
-
-#
-#   EJEMPLOS ANIDADOS
-#
-
-#if the_weather_is_good:
-#    if nice_restaurant_is_found:
-#        have_lunch()
-#    else:
-#        eat_a_sandwich()
-#else:
-#    if tickets_are_available:
-#        go_to_the_theater()
-#    else:
-#       go_shopping()
-
-
-#
-#   ELIF
-#
-
-#if the_weather_is_good:
-#    go_for_a_walk()
-#elif tickets_are_available:
-#    go_to_the_theater()
-#elif table_is_available:
-#    go_for_lunch()
-#else:
-#    play_chess_at_home()
-
-
-# EJECUCION
-# Se leen dos números
-number1 = int(input("Ingresa el primer número: "))
-number2 = int(input("Ingresa el segundo número: "))
-
-# Elige el número más grande
-if number1 > number2:
-    larger_number = number1 # Es posible colocar esto arriba
+operacion = input("¿Qué operación deseas hacer? ")
+if operacion == "Suma" or operacion == "suma":
+    print("La Suma de estos números es:", num_1 + num_2)
+elif operacion == "Resta" or operacion == "resta":
+    print("La Resta de estos números es:", num_1 - num_2)
+elif operacion == "Multiplicacion" or operacion == "multiplicacion":
+    print("La Multiplicación de estos números es:", num_1 * num_2)
+elif operacion == "Division" or operacion == "division":
+    print("La División de estos números es:", num_1 / num_2)
 else:
-    larger_number = number2 # Lo mismo
-
-# Imprime el resultado
-print("El número más grande es:", larger_number)
-
-# Otro ejemplo
-# Se leen tres números
-number1 = int(input("Ingresa el primer número: "))
-number2 = int(input("Ingresa el segundo número: "))
-number3 = int(input("Ingresa el tercer número: "))
-
-# Asumimos temporalmente que el primer número
-# es el más grande.
-# Lo verificaremos pronto.
-largest_number = number1
-
-# Comprobamos si el segundo número es más grande que el mayor número actual
-# y actualiza el número más grande si es necesario.
-if number2 > largest_number:
-    largest_number = number2
-
-# Comprobamos si el tercer número es más grande que el mayor número actual
-# y actualiza el número más grande si es necesario.
-if number3 > largest_number:
-    largest_number = number3
-
-# Imprime el resultado.
-print("El número más grande es:", largest_number)
-
-# Se leen tres números.
-number1 = int(input("Ingresa el primer número: "))
-number2 = int(input("Ingresa el segundo número: "))
-number3 = int(input("Ingresa el tercer número: "))
-
-# Verifica cuál de los números es el mayor
-# y pásalo a la variable largest_number.
-
-largest_number = max(number1, number2, number3)
-#Para buscar el número mayor de cualquier cantidada de argumentos se usa max(); también existe la función min().
-
-# Imprime el resultado.
-print("El número más grande es:", largest_number)
+    print("La Operación que quieres hacer no existe.")
